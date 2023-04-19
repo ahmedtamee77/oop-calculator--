470123e3417a291b1ee880aa00753a7f287b57b6
ahmedtamee <ahmedtamee75@gmail.com>
2023-04-19 08:11:15 +0200

    usefull recap i think bout inheritance 0-.-0

---
+++ recap/classes/Car.class.php
@@ -0,0 +1,8 @@
+<?php 
+class Car{
+    public $model ;
+    public $speed; 
+    public function turnOn(){
+        echo "the car is working now";
+    }
+}
---
+++ recap/includes/myautoload.inc.php
@@ -0,0 +1,11 @@
+<?php 
+spl_autoload_register('myautoload');
+function myautoload($classname){
+    $path="classes/";
+    $ext =".class.php";
+    $fullpath = $path.$classname.$ext; 
+    include $fullpath;
+    if(!file_exists($fullpath)){
+        return false;
+    }
+}
